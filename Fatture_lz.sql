cfa10a97889e76476d3851786e39664b0d775d39
Gaspar Torriero <gtorriero@sayit.ch>
2025-02-26 13:39:58 +0100

    Update Fatture_lz.sql
    COSMESI

--- Fatture_lz.sql
+++ Fatture_lz.sql
@@ -15,10 +15,8 @@ SELECT
       mm_datfin AS 'Data Fine',
       tb_descfam as 'Famiglia',
       tb_descove as 'contropartita'
-      
 FROM
       dbo.testmag 
-      
 INNER JOIN
       movmag
       ON tm_tipork=mm_tipork
@@ -35,12 +33,13 @@ LEFT JOIN dbo.anagra
 LEFT JOIN artico on mm_codart=ar_codart
 LEFT JOIN tabcfam on ar_famprod=tb_codcfam
 LEFT JOIN tabcove on mm_controp=tb_codcove
-where tm_datdoc>='2023-01-01'
-and (tm_tipork in ('A','E','N'))
+WHERE tm_datdoc>='2023-01-01'
+AND (tm_tipork in ('A','E','N'))
 
-union all
+UNION ALL
 
-select testmagd.tm_anno AS 'Anno',
+SELECT
+      testmagd.tm_anno AS 'Anno',
       testmagd.tm_datdoc AS 'Data',
       testmagd.tm_numdoc AS 'Numero',
       testmagd.tm_tipork,
@@ -56,23 +55,26 @@ select testmagd.tm_anno AS 'Anno',
       mm_datfin AS 'Data Fine',
       tb_descfam as 'Famiglia',
       tb_descove as 'contropartita'
-from 
-testmag as testmagd inner join 
-testmag on testmagd.tm_tipork=testmag.tm_tiporkfat
-and testmagd.tm_anno=testmag.tm_annfat
-and testmagd.tm_serie=testmag.tm_alffat 
-and testmagd.tm_numdoc=testmag.tm_numfat  left join movmag
+FROM 
+      testmag as testmagd 
+INNER JOIN
+      testmag
+      ON testmagd.tm_tipork=testmag.tm_tiporkfat
+AND testmagd.tm_anno=testmag.tm_annfat
+AND testmagd.tm_serie=testmag.tm_alffat 
+AND testmagd.tm_numdoc=testmag.tm_numfat  
+LEFT JOIN movmag
 on testmag.tm_tipork=mm_tipork
-and testmag.tm_anno=mm_anno
-and testmag.tm_serie=mm_serie
-and testmag.tm_numdoc=mm_numdoc
-      LEFT JOIN dbo.tabcage
-     ON testmagd.tm_codagen = dbo.tabcage.tb_codcage
-      left join commess on testmagd.tm_commeca=co_comme
-  LEFT JOIN dbo.anagra
+AND testmag.tm_anno=mm_anno
+AND testmag.tm_serie=mm_serie
+AND testmag.tm_numdoc=mm_numdoc
+LEFT JOIN dbo.tabcage
+      ON testmagd.tm_codagen = dbo.tabcage.tb_codcage
+LEFT JOIN commess on testmagd.tm_commeca=co_comme
+LEFT JOIN dbo.anagra
      ON co_conto = an_conto
-left join artico on mm_codart=ar_codart
-left join tabcfam on ar_famprod=tb_codcfam
-left join tabcove on mm_controp=tb_codcove
-where testmagd.tm_datdoc>='2023-01-01'
-and (testmagd.tm_tipork in ('D'))
+LEFT JOIN artico on mm_codart=ar_codart
+LEFT JOIN tabcfam on ar_famprod=tb_codcfam
+LEFT JOIN tabcove on mm_controp=tb_codcove
+WHERE testmagd.tm_datdoc>='2023-01-01'
+AND (testmagd.tm_tipork in ('D'))
